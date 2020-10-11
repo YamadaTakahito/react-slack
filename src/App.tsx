193d6379c774c7d476b10027674acae3aa806db2
Takahito Yamada <d_p.t.a.d.fighter@icloud.com>
2020-10-11 15:45:43 +0900

    add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from "react-router-dom";
+import Top from './components/Top';
+import About from './components/About';
+
 
 function App() {
   return (
-      <h1>hello world</h1>
+      <Router>
+          <ul>
+              <li><Link to="/">Top</Link></li>
+              <li><Link to="/about/">About</Link></li>
+          </ul>
+          <Route path="/" exact component={Top} />
+          <Route path="/about/" exact component={About} />
+      </Router>
   );
 }
 
